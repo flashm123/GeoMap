fa8d53280c4046b3b5ce26da168ceb6d332acf29
Mikhail Tortsev <59667317+flashm123@users.noreply.github.com>
2022-09-05 14:09:02 +0300

    Migrate to .NET 5.0 (#3)
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Add config files
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update packages.config
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Change .net version
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Migrate to .net 5.0
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Update geomap-desktop.yml
    
    * Fix pipeline

--- src/GeoMapApp/App.xaml.cs
+++ src/GeoMapApp/App.xaml.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
-namespace MapWpfApplication
+namespace GeoMapApp
 {
     /// <summary>
-    /// Interaction logic with App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
